e37d9fda76021452f4a598f46e103ceac55550a9
yoyo <duduyoyo63-dev@yahoo.fr>
2014-02-26 23:04:38 +0100

    init2

---
+++ marcopolo/src/main/site/index.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Marcopolo</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/bootstrap-responsive.css" rel="stylesheet">
+<link href="css/my-bootstrap.css" rel="stylesheet">
+<!--[if lt IE 9]><script src="js/html5.js"></script><![endif]-->
+<link href='http://fonts.googleapis.com/css?family=Lato:300'
+	rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<div id="mp-navbar">
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="span6">
+				<span class="color-highlight"><img
+					src="img/marcopolo-logo.png" alt=""></span>
+
+				<h3>l'agence photographique</h3>
+				<blockquote>
+					Marcopolo est une Agence d'Illustration Générale et de Reportage.<br>
+					Vous recherchez-une photo ?<br> Explorez notre fond.
+					Vous souhaitez une photo inédite ou introuvable ?<br>
+					Réalisons ensemble la photo que vous cherchez sur la base d’un prix photothèque, avec nos maquilleuses, nos stylistes, nos modèles, nos studios...
+					Contactez-nous.
+					<br> restaurez vos  photos !
+					
+				</blockquote>
+			</div>
+			<div class="span6">
+				<!--Start Carousel-->
+				<div id="myCarousel" class="carousel slide">
+					<div class="carousel-inner">
+						<div class="item active">
+							<img src="img/featured/1.jpg" alt="">
+						</div>
+						<div class="item">
+							<img src="img/featured/2.jpg" alt="">
+						</div>
+						<div class="item">
+							<img src="img/featured/3.jpg" alt="">
+						</div>
+						<div class="item">
+							<img src="img/featured/4.jpg" alt="">
+						</div>
+					</div>
+					<a class="left carousel-control" href="#myCarousel"
+						data-slide="prev"><img src="img/arrow.png" alt=""></a> <a
+						class="right carousel-control" href="#myCarousel"
+						data-slide="next"><img src="img/arrow2.png" alt=""></a>
+				</div>
+				<!--End Carousel-->
+			</div>
+		</div>
+		<hr>
+		<div class="row">
+			<div class="span4">
+				<h3>
+					Familles<small> By <a href="#">Srawat56</a></small>
+				</h3>
+				<a rel="lightbox" href="img/galerie/2.jpg"><img
+					src="img/galerie/2.jpg" alt=""></a>
+			</div>
+			<div class="span4">
+				<h3>
+					Enfants<small> By <a href="#">Paul venter</a></small>
+				</h3>
+				<a rel="lightbox" href="img/galerie/1.jpg"><img
+					src="img/galerie/1.jpg" alt=""></a>
+			</div>
+			<div class="span4">
+				<h3>
+					Paysages<small> By <a href="#">Paul venter</a></small>
+				</h3>
+				<a rel="lightbox" href="img/galerie/3.jpg"><img
+					src="img/galerie/3.jpg" alt=""></a>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="span4">
+				<h3>
+					Familles<small> By <a href="#">Srawat56</a></small>
+				</h3>
+				<a rel="lightbox" href="img/galerie/2.jpg"><img
+					src="img/galerie/2.jpg" alt=""></a>
+			</div>
+			<div class="span4">
+				<h3>
+					Enfants<small> By <a href="#">Paul venter</a></small>
+				</h3>
+				<a rel="lightbox" href="img/galerie/1.jpg"><img
+					src="img/galerie/1.jpg" alt=""></a>
+			</div>
+			<div class="span4">
+				<h3>
+					Paysages<small> By <a href="#">Paul venter</a></small>
+				</h3>
+				<a rel="lightbox" href="img/galerie/3.jpg"><img
+					src="img/galerie/3.jpg" alt=""></a>
+			</div>
+		</div>
+		<hr>
+		<div class="row">
+			<div class="span6 thumb-list">
+				<a rel="lightbox" href="img/thumb-small01.png"><img
+					src="img/thumb-small01.png" alt=""></a>
+				<h3>
+					<a href="#">This Should Be Your Title.</a>
+				</h3>
+				<p>This will be the description or something I guess. I don't
+					know. Your choice.You could summarize something here. Keep it
+					simple. Just a few short sentences. Maybe one large one? No, a few
+					short ones.</p>
+			</div>
+			<div class="span6 thumb-list">
+				<a rel="lightbox" href="img/thumb-small02.png"><img
+					src="img/thumb-small02.png" alt=""></a>
+				<h3>
+					<a href="#">This Should Be Your Title.</a>
+				</h3>
+				<p>This will be the description or something I guess. I don't
+					know. Your choice.</p>
+				<a href="#" class="btn">Details</a>
+			</div>
+		</div>
+		<div class="row">
+			<div class="span6 thumb-list">
+				<a rel="lightbox" href="img/thumb-small01.png"><img
+					src="img/thumb-small01.png" alt=""></a>
+				<h3>
+					<a href="#">This Should Be Your Title.</a>
+				</h3>
+				<p>This will be the description or something I guess. I don't
+					know. Your choice.</p>
+				<a href="#" class="btn">Details</a>
+			</div>
+			<div class="span6 thumb-list">
+				<a rel="lightbox" href="img/thumb-small02.png"><img
+					src="img/thumb-small02.png" alt=""></a>
+				<h3>
+					<a href="#">This Should Be Your Title.</a>
+				</h3>
+				<p>This will be the description or something I guess. I don't
+					know. Your choice.</p>
+				<a href="#" class="btn">Details</a>
+			</div>
+		</div>
+		<hr>
+		<footer class="row">
+			<p>
+				&copy;2012 Your Company.<br> Design by <a
+					href="http://www.awfulmedia.com">AwfulMedia.com</a>
+			</p>
+		</footer>
+	</div>
+	<!-- /container -->
+	<script src="js/jquery-1.7.1.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap-transition.js" type="text/javascript"></script>
+	<script src="js/bootstrap-carousel.js" type="text/javascript"></script>
+	<script src="js/bootstrap-alert.js" type="text/javascript"></script>
+	<script src="js/bootstrap-modal.js" type="text/javascript"></script>
+	<script src="js/bootstrap-dropdown.js" type="text/javascript"></script>
+	<script src="js/bootstrap-scrollspy.js" type="text/javascript"></script>
+	<script src="js/bootstrap-tab.js" type="text/javascript"></script>
+	<script src="js/bootstrap-tooltip.js" type="text/javascript"></script>
+	<script src="js/bootstrap-popover.js" type="text/javascript"></script>
+	<script src="js/bootstrap-button.js" type="text/javascript"></script>
+	<script src="js/bootstrap-collapse.js" type="text/javascript"></script>
+	<script src="js/bootstrap-typeahead.js" type="text/javascript"></script>
+	<script src="js/jquery-ui-1.8.18.custom.min.js" type="text/javascript"></script>
+	<script src="js/jquery.smooth-scroll.min.js" type="text/javascript"></script>
+	<script src="js/lightbox.js" type="text/javascript"></script>
+	<script type="text/javascript">
+$('.carousel').carousel({
+  interval: 5000
+});
+
+$(function(){
+    $("#mp-navbar").load("mp-navbar.html"); 
+  });
+</script>
+</body>
+</html>

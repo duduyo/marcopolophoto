43fae46a069944a44395cd6fed7f1065cf45240b
duduyo <duduyoyo63-dev@yahoo.fr>
2014-02-28 12:25:39 +0100

    path

--- marcopolo/src/main/site/nosphotos.php
+++ marcopolo/src/main/site/nosphotos.php
@@ -15,6 +15,22 @@
 	</div>
 <div class="container">
   <hr>
+  
+<?php
+
+$dir    = './img/galerie';
+$files2 = scandir($dir, 1);
+$colonne=1;
+
+foreach ($files2 as &$value) {
+?>
+
+
+<?php
+$colonne++;
+}
+?>
+  
   <div class="row">
     <div class="span4">
       <h3>Deep Sky <small> By <a href="#">Srawat56</a></small></h3>
@@ -49,44 +65,9 @@
       <a rel="lightbox" href="img/thumb1.jpg"><img src="img/thumb1.jpg" alt=""></a> </div>
   </div>
   <hr>
+
+  
   <footer class="row">
-    <div>
-      <div class="span4">
-        <div class="is-padded">
-          <nav>
-            <h2>Navigation</h2>
-            <hr>
-            <ul>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">Services</a></li>
-              <li><a href="#">Portfolio</a></li>
-              <li><a href="#">Contact</a></li>
-              <li><a href="#">About</a></li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-      <div class="span4">
-        <div class="is-padded">
-          <h2>Twitter</h2>
-          <hr>
-          <a href="#">@AwfulMedia</a>
-          <p>This is what my tweet looks like on this website. Tweet tweet. Twitter Twatter.</p>
-          <em>3 days ago</em><br>
-          <br>
-          <a href="#">@AwfulMedia</a>
-          <p>This is what my tweet looks like on this website. Tweet tweet. Twitter Twatter.</p>
-          <em>3 days ago</em> </div>
-      </div>
-      <div class="span4">
-        <div class="is-padded">
-          <h2>Details</h2>
-          <blockquote>Respond is a fully responsive website template utilizing Twitter's Bootstrap framework. What does that mean? First of all, the website will respond according to the size of the viewers browser window. Go on, try it out. Resize your window. On top of that, the framework offers loads of ready-to-go features. Check it out with the button below. <br>
-            <br>
-            <em>- AwfulMedia.com</em> </blockquote>
-        </div>
-      </div>
-    </div>
     <p> &copy;2012 Your Company.<br>
       Design by <a href="http://www.awfulmedia.com">AwfulMedia.com</a> </p>
   </footer>
